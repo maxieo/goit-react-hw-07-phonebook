c0286d2a7ff8463693c77664b5a55be89a76637b
maxieo <97198812+maxieo@users.noreply.github.com>
2022-12-01 18:10:30 +0200

    olala

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,16 +1,4 @@
-import { createSelector } from "@reduxjs/toolkit";
-
 export const getContacts = state => state.contacts.contacts
 export const getFilter = state => state.filter
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
-
-
-export const visibleContacts = createSelector(
-  [getContacts, getFilter],
-  (contacts, filterValue) => { 
-    return contacts.filter(contact => { 
-      return contact.name.toLowerCase().includes(filterValue.toLowerCase())
-    })
-  }
-)
